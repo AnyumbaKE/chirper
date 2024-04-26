67aeed8b87ba5265370cbe1f0f76ca24c296a455
AnyumbaKE <esanyumba@gmail.com>
2024-04-26 18:05:11 +0300

    chirps.store update

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
     public function index(): View
     {
         return view('chirps.index');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validate = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validate);
+
+        return redirect()->route('chirps.index');
+
     }
 }
